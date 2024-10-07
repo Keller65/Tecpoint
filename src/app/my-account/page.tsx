0d730cb5fb2eebe3cb50e147ae049bcf7819eae7
Aerley <lopezkkler65@gmail.com>
2024-10-07 16:39:38 -0600

    Creaccion de paginas

---
+++ src/app/my-account/page.tsx
@@ -0,0 +1,16 @@
+
+function LoginPage() {
+  return (
+    <main className="flex bg-[#f3f3f3] w-full h-screen">
+      <section className="h-full w-[35vw]">
+        <div className="w-full h-screen flex justify-center 2xl:p-8">
+          <h1 className="font-[Poppins] text-xl text-center">Bienvenido de Nuevo!</h1>
+        </div>
+      </section>
+
+      <section className="bg-red-300 flex-1"></section>
+    </main>
+  )
+}
+
+export default LoginPage
---
+++ src/components/menu/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+function NavbarMenu() {
+  const url = "https://wikirock.net/wp-content/uploads/2023/06/Lana-Del-Rey.jpg"
+  return (
+    <nav className="flex items-center justify-between w-full px-8 py-4">
+      <Image alt="Tecpoint Logo" src="/logo.png" width={180} height={80} />
+      <p>Welcome to Tecpoint website</p>
+
+      <Image
+        className="h-10 w-10 rounded-full aspect-square object-cover"
+        alt="User profile photo"
+        src={url}
+        width={80} height={80}
+      />
+    </nav>
+  )
+}
+
+export default NavbarMenu
